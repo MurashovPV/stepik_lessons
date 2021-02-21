a471c81a8a48e0a899f8cd48637224aa43586a35
MurashovPV <pavel.murashov@rubius.com>
2021-02-21 20:44:40 +0700

    Changed email_seed variable naming, used variables to store web-elements

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -2,15 +2,13 @@ from selenium import webdriver
 import random
 
 start_page_url = "http://selenium1py.pythonanywhere.com/ru/"
-
-alert_success_message_locator = ".wicon"
-login_link_locator = "#login_link"
+successful_registration_text = "Спасибо за регистрацию!"
 
 
 def verify_that_user_can_register_with_correct_credentials():
     # Data
-    random_number_for_generating_unique_email = random.randint(1, 9999)
-    test_email = f"test{random_number_for_generating_unique_email}@test.kz"
+    email_seed = random.randint(2, 9999)
+    test_email = f"test{email_seed}@test.kz"
     test_password = "ThisPasswordShouldBeFine"
 
     try:
@@ -18,26 +16,27 @@ def verify_that_user_can_register_with_correct_credentials():
         browser = webdriver.Chrome()
         browser.implicitly_wait(5)
         browser.get(start_page_url)
-        email_input_locator = "#id_registration-email"
-        password_input_locator = "#id_registration-password1"
-        confirm_password_input_locator = "#id_registration-password2"
-        register_button_locator = "[name='registration_submit']"
-        successful_registration_text = "Спасибо за регистрацию!"
 
-        browser.find_element_by_css_selector(login_link_locator).click()
-        browser.find_element_by_css_selector(email_input_locator).clear()
-        browser.find_element_by_css_selector(password_input_locator).clear()
-        browser.find_element_by_css_selector(confirm_password_input_locator).clear()
+        login_link = browser.find_element_by_css_selector('#login_link')
+        login_link.click()
+        email_input = browser.find_element_by_css_selector("#id_registration-email")
+        password_input = browser.find_element_by_css_selector("#id_registration-password1")
+        confirm_password_input = browser.find_element_by_css_selector("#id_registration-password2")
+        register_button = browser.find_element_by_css_selector("[name='registration_submit']")
+
+        email_input.clear()
+        password_input.clear()
+        confirm_password_input.clear()
 
         # Act
-        browser.find_element_by_css_selector(email_input_locator).send_keys(test_email)
-        browser.find_element_by_css_selector(password_input_locator).send_keys(test_password)
-        browser.find_element_by_css_selector(confirm_password_input_locator).send_keys(test_password)
+        email_input.send_keys(test_email)
+        password_input.send_keys(test_password)
+        confirm_password_input.send_keys(test_password)
 
         # Assert
-        browser.find_element_by_css_selector(register_button_locator).click()
-        actual_message_displayed = browser.find_element_by_css_selector(alert_success_message_locator).text
-        assert successful_registration_text in actual_message_displayed, \
+        register_button.click()
+        alert_message_displayed = browser.find_element_by_css_selector(".wicon").text
+        assert successful_registration_text in alert_message_displayed, \
             "Text informing about successful registration was not displayed"
 
     finally:
@@ -45,3 +44,9 @@ def verify_that_user_can_register_with_correct_credentials():
 
 
 verify_that_user_can_register_with_correct_credentials()
+
+'''
+Не смог в рамках одного .py файла вынести элементы с локаторами за рамки теста, т.к. веб-элементы не находились 
+до того момента как был совершён переход на содержащую их страницу. Из доп.замечаний только вынес веб-элементы 
+в отдельные переменные.
+'''
