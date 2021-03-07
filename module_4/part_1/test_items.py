12bc772a98f73c9bba5e370691846f7a1a234423
MurashovPV <pavel.murashov@rubius.com>
2021-03-07 10:38:18 +0700

    Cosmetic fixes in part 1

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,15 +1,15 @@
 import random
 
-add_to_basket_translations = {"en-GB": "Add to basket",
-                              "ru": "Добавить в корзину",
-                              "es": "Añadir al carrito",
-                              "fr": "Ajouter au panier"}
+stash = {"en-GB": "Add to basket",
+         "ru": "Добавить в корзину",
+         "es": "Añadir al carrito",
+         "fr": "Ajouter au panier"}
 
 
 def test_add_to_basket_button_should_contain_correct_value(browser, language):
     # Arrange
-    catalogue_link = f"http://selenium1py.pythonanywhere.com/{language.lower()}/catalogue"
-    browser.get(catalogue_link)
+    link = f"http://selenium1py.pythonanywhere.com/{language.lower()}/catalogue"
+    browser.get(link)
     goods_list = browser.find_elements_by_css_selector("h3")
 
     # Act
@@ -17,5 +17,4 @@ def test_add_to_basket_button_should_contain_correct_value(browser, language):
 
     # Assert
     add_to_basket_button = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert add_to_basket_button.text == add_to_basket_translations[language], "There is wrong text displayed on " \
-                                                                              "'Add to Basket button'"
+    assert add_to_basket_button.text == stash[language], "Something wrong displayed on button"
