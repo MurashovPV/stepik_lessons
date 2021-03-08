7d195a649c4ad541eb69861efacafb9b5b45632a
MurashovPV <pavel.murashov@rubius.com>
2021-03-08 22:51:11 +0700

    Setting up the project

---
+++ module_5/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en-GB',
+                     help="Choose language: 'ru' or 'en-GB' or 'es' or 'fr'")
+
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+
+@pytest.fixture()
+def language(request):
+    return request.config.getoption("language").lower()
