1f25c34b8f6d0c6a26aeaf52c3112fa8967f1f8c
MurashovPV <pavel.murashov@rubius.com>
2021-03-20 23:07:55 +0700

    Added methods to work with basket; checking is elements disappeared or not present.

--- module_5/pages/base_page.py
+++ module_5/pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
 import math
 
@@ -10,8 +13,27 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def get_basket_total(self):
+        product_price = ""
+        basket_total = self.browser.find_element(*BasePageLocators.BASKET_TOTAL).text
+        for symbol in basket_total:
+            if symbol.isdigit():
+                product_price += symbol
+            elif symbol == "." or symbol == ",":
+                product_price += "."
+        return float(product_price)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -19,6 +41,23 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -34,12 +73,6 @@ class BasePage:
         except NoAlertPresentException:
             print("No second alert presented")
 
-    def get_basket_total(self):
-        product_price = ""
-        basket_total = self.browser.find_element(*BasePageLocators.BASKET_TOTAL).text
-        for symbol in basket_total:
-            if symbol.isdigit():
-                product_price += symbol
-            elif symbol == "." or symbol == ",":
-                product_price += "."
-        return float(product_price)
+    def view_basket_page(self):
+        basket_link = self.browser.find_element(*BasePageLocators.VIEW_BASKET)
+        basket_link.click()
