3eb80d272d72ee02bfd6b4bb1e129718a2c5c1c0
MurashovPV <pavel.murashov@rubius.com>
2021-03-20 23:10:32 +0700

    Added taking screenshots functionality. Updated browser and language options.

--- module_5/conftest.py
+++ module_5/conftest.py
@@ -1,21 +1,36 @@
 import pytest
 from selenium import webdriver
+from datetime import datetime
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en-GB',
                      help="Choose language: 'ru' or 'en-GB' or 'es' or 'fr'")
-    parser.addoption('--browser_name', action='store', default='chrome',
+    parser.addoption('--browser', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
+    print("\nquit browser..")
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    browser.save_screenshot('screenshots\\screenshot-%s.png' % now)
     browser.quit()
-
-
-@pytest.fixture()
-def language(request):
-    return request.config.getoption("language")
