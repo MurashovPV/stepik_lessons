d703193d600e80326353058603eaf7d5a12048bc
MurashovPV <pavel.murashov@rubius.com>
2021-03-20 23:16:17 +0700

    Added a test for empty basket.

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,10 +1,13 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-class TestMainPage:
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         page = MainPage(browser, link)
         page.open()
@@ -12,6 +15,16 @@ class TestMainPage:
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.view_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_empty_basket()
+        basket_page.should_display_message_regarding_empty_basket()
+
+
+class TestMainPage:
     def test_guest_should_see_login_link(self, browser):
         page = MainPage(browser, link)
         page.open()
